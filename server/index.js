10b194f8047f0bca6b71c04522a9410a7f689808
uurhruejeirhejj-max <uurhruejeirhejj@gmail.com>
2026-05-13 15:24:17 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const corsOptions = {
     
     const allowedOrigins = [
       'http://localhost:8080',
-      'http://127.0.0.1:8080',
+      'https://spacesurvivor0.netlify.app',
       'http://localhost:5500',
       'http://127.0.0.1:5500',
       'http://localhost:3000',
